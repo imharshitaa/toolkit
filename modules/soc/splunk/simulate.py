1d4624ae501ffd5cef60d774c05b3a068c4d1871
Harshitaa <143587481+imharshitaa@users.noreply.github.com>
2026-02-11 15:42:48 +0530

    Create simulate.py

---
+++ modules/soc/splunk/simulate.py
@@ -0,0 +1,7 @@
+from rich import print
+
+def run(target):
+    print(f"Simulating log attack from {target}")
+    print("Generating failed login attempts...")
+    print("Triggering alert rule...")
+    print("Alert successfully created in Splunk.")
