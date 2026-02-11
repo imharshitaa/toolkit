572fa42080199b622bda4472028f3ff4f57843a0
Harshitaa <143587481+imharshitaa@users.noreply.github.com>
2026-02-11 15:42:31 +0530

    Create deploy.py

---
+++ modules/soc/splunk/deploy.py
@@ -0,0 +1,7 @@
+from rich import print
+
+def run():
+    print("Setting up Splunk server...")
+    print("Configuring log ingestion...")
+    print("Creating dashboards...")
+    print("Splunk deployment completed.")
